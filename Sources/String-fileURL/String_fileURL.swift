1e4002c7759d80f150beb64ff16ca23164b15ee0
David E Nedrow <david.e.nedrow@jpmorgan.com>
2019-04-06 23:52:46 -0400

    Added string extension for URL.

--- Sources/String-fileURL/String_fileURL.swift
+++ Sources/String-fileURL/String_fileURL.swift
@@ -1,3 +1,23 @@
-struct String_fileURL {
-    var text = "Hello, World!"
+import Foundation
+
+extension String {
+    /// Returns the URL of the currently executing file.
+    var fileURL: URL {
+        return URL(fileURLWithPath: self)
+    }
+
+    /// Returns just the extension of the currently executing file.
+    var pathExtension: String {
+        return fileURL.pathExtension
+    }
+
+    /// Returns the name of the currently executing file.
+    var lastPathComponent: String {
+        return fileURL.lastPathComponent
+    }
 }
+
+//struct String_fileURL {
+//    var text = "Hello, World!"
+//}
+//
